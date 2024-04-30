9b9007837b92168a0acb5361656ae71317de8d6f
dam_lmj <juanvi1921.cursos@gmail.com>
2024-04-30 13:10:11 +0200

    commit con el segundo test de zapping y clase zapping mejorada

--- Zapping/src/Zapping.java
+++ Zapping/src/Zapping.java
@@ -1,21 +1,24 @@
 public class Zapping {
-    private static int canal = 0;
+    private static int canal = 1;
+    private static int numClics = 0;
     private static final int K_NUMCANALES = 100;
     public static void main(String[] args) {
         cambiarCanal(20, 50);
     }
 
     public static int siguenteCanal() {
+        numClics++;
         return canal++;
     }
 
     public static int anteriorCanal() {
+        numClics++;
         return canal--;
     }
 
     public static int cambiarCanal(int canalOrigen, int canalDestino) {
-        int numClics = 0;
-        for (int i = 1; i < K_NUMCANALES; i++) {
+
+        for (int i = 0; i < K_NUMCANALES; i++) {
             if (canalDestino > canalOrigen) {
                 siguenteCanal();
                 if (canal == canalDestino) {
--- Zapping/src/ZappingTest.java
+++ Zapping/src/ZappingTest.java
@@ -3,8 +3,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ZappingTest {
+    /*
     @Test
     public void testZapping1(){
         assertEquals(0, Zapping.cambiarCanal(1,1));
+    }
+
+     */
+
+    @Test
+    public void testZapping2() {
+        assertEquals(1, Zapping.cambiarCanal(1,2));
     }
 }
