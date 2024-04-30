28727b3f6669a59e43cda0b568fb395edfb5c2f0
Juanvi <juanvi1921.cursos@gmail.com>
2024-04-30 16:21:19 +0200

    Se ha refactorizado y cambiado el metodo cambiarCanal, test pasados.

--- Zapping/src/Zapping.java
+++ Zapping/src/Zapping.java
@@ -1,37 +1,17 @@
 public class Zapping {
-    private static int canal = 1;
-    private static int numClics = 0;
-    private static final int K_NUMCANALES = 100;
+    private static final int K_NUMCANALES = 99;
     public static void main(String[] args) {
-        cambiarCanal(20, 50);
-    }
 
-    public static int siguenteCanal() {
-        numClics++;
-        return canal++;
-    }
-
-    public static int anteriorCanal() {
-        numClics++;
-        return canal--;
     }
 
     public static int cambiarCanal(int canalOrigen, int canalDestino) {
-        canal = canalOrigen;
-        if (canalDestino != canalOrigen) {
-            for (int i = 0; i < K_NUMCANALES; i++) {
-                if ((canalDestino - canalOrigen) > 0 && canalDestino > canalOrigen) {
-                    siguenteCanal();
-                    if (canal == canalDestino) {
-                        return numClics;
-                    }
-                } else if ((canalOrigen - canalDestino) < 0 && canalDestino < canalOrigen) {
-                    anteriorCanal();
-                    if (canal == canalDestino) {
-                        return numClics;
-                    }
-                }
-            }
+        int distanciaDelante = Math.abs(canalDestino - canalOrigen);
+        int distanciaAtras = K_NUMCANALES - distanciaDelante;
+        int numClics = 0;
+        if (distanciaDelante <= distanciaAtras) {
+            numClics = distanciaDelante;
+        } else {
+            numClics = distanciaAtras;
         }
         return numClics;
     }
