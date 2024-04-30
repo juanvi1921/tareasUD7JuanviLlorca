cbca9899ea7a089554192aa0ba711554b0daecc5
Juanvi <juanvi1921.cursos@gmail.com>
2024-04-30 17:29:55 +0200

    Tercer commit de Divisible3 con el tercer test pasado.

--- DivisiblePor3/src/Divisible3Test.java
+++ DivisiblePor3/src/Divisible3Test.java
@@ -12,4 +12,9 @@ class Divisible3Test {
     public void divisiblePor3Test2() {
         assertEquals("SI", Divisible3.compruebaDivisiblePor3(6));
     }
+
+    @Test
+    public void divisiblePor3Test3() {
+        assertEquals("NO", Divisible3.compruebaDivisiblePor3(4));
+    }
 }
